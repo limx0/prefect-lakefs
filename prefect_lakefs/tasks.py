960544811b517d74ac3ad94cb7dbc04f2a64bbc2
brad <brad@edgestackers.com>
2023-05-10 17:09:30 +1000

    Add simple test for storage, remove interrogate

--- prefect_lakefs/tasks.py
+++ prefect_lakefs/tasks.py
@@ -1,3 +1,4 @@
+# noqa
 from prefect_lakefs.branches import (
     cherry_pick,
     create_branch,
@@ -8,3 +9,14 @@ from prefect_lakefs.branches import (
     reset_branch,
     revert_branch,
 )
+
+__all__ = [
+    "cherry_pick",
+    "create_branch",
+    "delete_branch",
+    "diff_branch",
+    "get_branch",
+    "list_branches",
+    "reset_branch",
+    "revert_branch",
+]
---
+++ tests/test_storage.py
@@ -0,0 +1,25 @@
+from unittest.mock import patch
+
+from prefect import flow, task
+
+from prefect_lakefs.storage import LakeFS, LakeFSCredentials
+
+
+@patch("prefect_lakefs.storage.LakeFS._write_sync")
+def test_storage_init(mock_write_sync):
+    @task(
+        result_storage=LakeFS(
+            bucket_name="bucket_name", credentials=LakeFSCredentials()
+        ),
+        persist_result=True,
+    )
+    def my_task():
+        return 1
+
+    @flow
+    def my_flow():
+        my_task()
+
+    my_flow()
+
+    assert mock_write_sync.call_count == 1
